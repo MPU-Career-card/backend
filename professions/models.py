ded4da6839d5c221f5aa3225faf258dd0dbd4900
Mikhail Sergeev <sergeevm100@gmail.com>
2024-04-14 07:49:53 +0300

    speciality cleared

--- professions/models.py
+++ professions/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Professions(models.Model):
     title = models.CharField(max_length=255)
+    image_link = models.URLField(verbose_name='Ссылка на главное изображение')
+    description = models.TextField(verbose_name='Описание профессии')
 
     def __str__(self):
         return self.title
@@ -11,21 +13,18 @@ class Professions(models.Model):
         verbose_name = 'Профессия'
         verbose_name_plural = 'Профессии'
 
+
 class Tag(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
     name = models.TextField(verbose_name='Имя тэга')
 
-class Promo(models.Model):
-    profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
-    image = models.ImageField(upload_to='promo_images/', verbose_name='Фото промо')
-    description = models.TextField(verbose_name='Описание профессии')
-
     def __str__(self):
-        return self.title
+        return self.name
 
     class Meta:
-        verbose_name = 'Промо'
-        verbose_name_plural = 'Промо'
+        verbose_name = 'Tэг'
+        verbose_name_plural = 'Тэги'
+
 
 class Task(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
@@ -59,6 +58,7 @@ class Skill(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название навыка')
     description = models.TextField(verbose_name='Описание навыка')
     svg = models.TextField(verbose_name='SVG иконка')
+
     def __str__(self):
         return self.name
 
@@ -66,7 +66,8 @@ class Skill(models.Model):
         verbose_name = 'Навык'
         verbose_name_plural = 'Навыки'
 
-class Bakalavr(models.Model):
+
+class Bachelor(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
     faculty_name = models.CharField(max_length=255, verbose_name='Название факультета')
     speciality_name = models.CharField(max_length=255, verbose_name='Название специальности')
@@ -74,10 +75,13 @@ class Bakalavr(models.Model):
 
     def __str__(self): 
         return self.speciality_name
+
     class Meta:
         verbose_name = 'Бакалавриат'
         verbose_name_plural = 'Бакалавриат'
-class Magistr(models.Model):
+
+
+class Master(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
     faculty_name = models.CharField(max_length=255, verbose_name='Название факультета')
     speciality_name = models.CharField(max_length=255, verbose_name='Название специальности')
@@ -85,11 +89,13 @@ class Magistr(models.Model):
 
     def __str__(self): 
         return self.speciality_name
+
     class Meta:
         verbose_name = 'Магистратура'
         verbose_name_plural = 'Магистратура'
 
-class DPO(models.Model):
+
+class FurtherEducation(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
     faculty_name = models.CharField(max_length=255, verbose_name='Название факультета')
     speciality_name = models.CharField(max_length=255, verbose_name='Название специальности')
@@ -97,6 +103,7 @@ class DPO(models.Model):
 
     def __str__(self): 
         return self.speciality_name
+
     class Meta:
         verbose_name = 'ДПО'
         verbose_name_plural = 'ДПО'
@@ -104,7 +111,7 @@ class DPO(models.Model):
 
 class HHVacancy(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
-    link = models.CharField(max_length=255,verbose_name='Ссылка на вакансию')
+    link = models.URLField(verbose_name='Ссылка на вакансию')
 
     def __str__(self):
         return self.title
@@ -112,10 +119,13 @@ class HHVacancy(models.Model):
     class Meta:
         verbose_name = 'Вакансия HH'
         verbose_name_plural = 'Вакансии HH'
+
+
 class Partners(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
     name = models.CharField(max_length=255, verbose_name='Название партнера')
     link = models.URLField(verbose_name='Ссылка на партнера')
+    image_link = models.URLField(verbose_name='Ссылка на изображение')
 
     def __str__(self):
         return self.name
@@ -124,9 +134,11 @@ class Partners(models.Model):
         verbose_name = 'Партнер'
         verbose_name_plural = 'Партнеры'
     
+
 class UsefulLink(models.Model):
     profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
     name = models.CharField(max_length=255, verbose_name='Название ссылки')
+    description = models.TextField(verbose_name='Описание')
     link = models.URLField(verbose_name='Ссылка')
 
     def __str__(self):
@@ -135,35 +147,3 @@ class UsefulLink(models.Model):
     class Meta:
         verbose_name = 'Полезная ссылка'
         verbose_name_plural = 'Полезные ссылки'
-class Card(models.Model):
-    profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
-    title = models.CharField(max_length=255, verbose_name='Название карточки')
-    text = models.TextField(verbose_name='Текст карточки')
-    image = models.ImageField(upload_to='card_images/', verbose_name='Фото карточки')
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Карточка'
-        verbose_name_plural = 'Карточки'
-
-class Speciality(models.Model):
-    profession = models.ForeignKey(Professions, on_delete=models.CASCADE, verbose_name='Профессия')
-    academic_degree = models.CharField(max_length=255, verbose_name='Академическая степень')
-    name = models.CharField(max_length=255, verbose_name='Название специальности')
-    faculty = models.CharField(max_length=255, verbose_name='Факультет')
-    budget_places_count = models.IntegerField(verbose_name='Бюджетные места')
-    year_budget_places_count = models.IntegerField(null=True, verbose_name='Год бюджетных мест')
-    passing_score = models.IntegerField(verbose_name='Количество мест')
-    year_passing_score = models.IntegerField(null=True, verbose_name='Год количества мест')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена обучения')
-    year_price = models.IntegerField(null=True, verbose_name='Год цены обучения')
-    period = models.IntegerField(verbose_name='Период обучения')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Академическая степень'
-        verbose_name_plural = 'Академические степени'
--- professions/serializers.py
+++ professions/serializers.py
@@ -1,86 +1,80 @@
 from rest_framework import serializers
-from .models import Professions, Promo, Task, Card, Speciality, Tag, MapPointer, Skill, Bakalavr, Magistr, DPO, HHVacancy, Partners, UsefulLink
+from .models import Professions, Task, Tag, MapPointer, Skill, Bachelor, Master, FurtherEducation, HHVacancy, Partners, UsefulLink
+
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
 
-class PromoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Promo
-        fields = '__all__'
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
 
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Card
-        fields = '__all__'
-
-class SpecialitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Speciality
-        fields = '__all__'
 
 class MapPointerSerializer(serializers.ModelSerializer):
     class Meta:
         model = MapPointer
         fields = '__all__'
 
+
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
         fields = '__all__'
 
-class BakalavrSerializer(serializers.ModelSerializer):
+
+class BachelorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Bakalavr
+        model = Bachelor
         fields = '__all__'
 
-class MagistrSerializer(serializers.ModelSerializer):
+
+class MasterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Magistr
+        model = Master
         fields = '__all__'
 
-class DPOSerializer(serializers.ModelSerializer):
+
+class FurtherEducationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = DPO
+        model = FurtherEducation
         fields = '__all__'
+
 
 class HHVacancySerializer(serializers.ModelSerializer):
     class Meta:
         model = HHVacancy
         fields = '__all__'
 
+
 class PartnersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Partners
         fields = '__all__'
+
 
 class UsefulLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsefulLink
         fields = '__all__'
 
+
 class ProfessionsSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
-    promos = PromoSerializer(many=True, read_only=True)
     tasks = TaskSerializer(many=True, read_only=True)
-    cards = CardSerializer(many=True, read_only=True)
-    specialities = SpecialitySerializer(many=True, read_only=True, source='speciality_set')
     map_pointers = MapPointerSerializer(many=True, read_only=True, source='mappointer_set')
     skills = SkillSerializer(many=True, read_only=True, source='skill_set')
-    bakalavrs = BakalavrSerializer(many=True, read_only=True, source='bakalavr_set')
-    magistrs = MagistrSerializer(many=True, read_only=True, source='magistr_set')
-    dpos = DPOSerializer(many=True, read_only=True, source='dpo_set')
+    bachelors = BachelorSerializer(many=True, read_only=True, source='bachelor_set')
+    masters = MasterSerializer(many=True, read_only=True, source='master_set')
+    further_ecuations = FurtherEducationSerializer(many=True, read_only=True, source='further_education_set')
     hh_vacancies = HHVacancySerializer(many=True, read_only=True, source='hhvacancy_set')  
     partners = PartnersSerializer(many=True, read_only=True, source='partners_set')
     useful_links = UsefulLinkSerializer(many=True, read_only=True, source='usefullink_set')
 
     class Meta:
         model = Professions
-        fields = ('id', 'title', 'tags', 'promos', 'tasks', 'cards', 'specialities', 'map_pointers', 'skills', 'bakalavrs', 'magistrs', 'dpos', 'hh_vacancies', 'partners', 'useful_links')
+        fields = ('id', 'title', 'tags', 'tasks', 'map_pointers', 'skills', 'bachelors',
+                  'masters', 'further_ecuations', 'hh_vacancies', 'partners', 'useful_links')
